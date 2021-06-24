e4bc933425ef06b6a279c827600f9c8b07f6d886
Gabriel Birke <gabriel.birke@posteo.net>
2021-06-24 14:28:01 +0200

    Testing with mobile devices in example code

--- multiremote.js
+++ multiremote.js
@@ -14,6 +14,7 @@ export const CONNECTION = {
 	connectionRetryCount: 1
 }
 
+/*
 const capabilities = {
 		ie11: {capabilities: {platformName: 'WIN7', browserName: 'internet explorer', browserVersion: 11}, ...CONNECTION},
 		edge: {capabilities: {platformName: 'WIN10', browserName: 'microsoftedge', browserVersion: 91}, ...CONNECTION},
@@ -25,13 +26,30 @@ const capabilities = {
 	}
 ;
 
+ */
+const capabilities = {
+
+	'samsung_s10': {capabilities: { browserName : 'Chrome',platform : 'Android', browserVersion : '10.0',platformName : 'Android', deviceName : 'Galaxy S10'},  ...CONNECTION},
+	'samsung_s10_landscape': {capabilities: { browserName : 'Chrome',platform : 'Android', browserVersion : '10.0',platformName : 'Android', deviceName : 'Galaxy S10', orientation: "LANDSCAPE"},  ...CONNECTION},
+
+	'nexus_6': {capabilities: { browserName : 'browser',platform : 'Android', browserVersion : '6.0',platformName : 'Android', deviceName : 'Nexus 6'},  ...CONNECTION},
+
+	 'iphone_xs_max': {capabilities: { browserName : 'safari', platform : 'iOS', browserVersion : '12.1', platformName : 'iOS', deviceName : 'iPhone XS Max' },  ...CONNECTION},
+	 'iphone_xs_max_landscape': {capabilities: { browserName : 'safari', platform : 'iOS', browserVersion : '12.1', platformName : 'iOS', deviceName : 'iPhone XS Max', orientation: "LANDSCAPE" },  ...CONNECTION},
+
+	'iphone_se': {capabilities: { browserName : 'safari', platform : 'iOS', browserVersion : '11.4', platformName : 'iOS', deviceName : 'iPhone SE' },  ...CONNECTION},
+	'iphone_se_landscape': {capabilities: { browserName : 'safari', platform : 'iOS', browserVersion : '11.4', platformName : 'iOS', deviceName : 'iPhone SE', orientation: "LANDSCAPE" },  ...CONNECTION},
+
+};
+
 (async () => {
 	const browsers = await multiremote( capabilities );
 	// TODO check with smaller browser set if it works when the directory does not exist
 	const imageWriter = await createImageWriter( path.join( __dirname, 'banner-shots/test' ) );
 
 	const shoot = async browser => {
-		const URL = 'https://de.m.wikipedia.org/wiki/Wikipedia:Hauptseite?banner=B20_WMDE_Test_05_var';
+		// const URL = 'https://de.m.wikipedia.org/wiki/Wikipedia:Hauptseite?banner=B20_WMDE_Test_05_var';
+		const URL = 'https://de.m.wikipedia.org/wiki/Wikipedia:Hauptseite?banner=B20_WMDE_Test_Mobile';
 		const start = new Date();
 
 		await browsers[browser].url( URL )
