9c54122699c0f1dae413320a6629bc73e317ee57
Gabriel Birke <gabriel.birke@posteo.net>
2022-08-25 11:31:32 +0200

    Add a campaign mapper
    Create a function that maps branch names to campaign (channel) names
    from campaign_info.toml

---
+++ src/determineCampaignFromBranchName.ts
@@ -0,0 +1,32 @@
+/**
+ * This function checks a branch or tag name like C21_WMDE_Test_07 and 
+ * returns a channel name for the campaign_info.toml file.
+ */
+export function determineCampaignFromBranchName( branchName: string ): string {
+	const campaignParts = new Map( branchName.
+								  toLowerCase().
+								  split("_").
+								  map( part => [ part, true ] ) 
+								 );
+	if ( !campaignParts.has('wmde') && !campaignParts.has('wpde') ) {
+		throw new Error('Name does not contain WPDE or WMDE');
+	}
+
+	if ( campaignParts.has('wpde') ) {
+		if ( Array.from( campaignParts.keys() ).find( k => /mob/.test(k) ) ) {
+			return 'wikipediade_mobile';
+		}
+		return 'wikipediade';
+	}
+
+	if ( campaignParts.has('mobile') ) {
+		return campaignParts.has('en') ? 'mobile_english' : 'mobile';
+	}
+	
+	if ( campaignParts.has('ipad') || campaignParts.has('pad') ) {
+		return campaignParts.has('en') ? 'pad_en' : 'pad';
+	}
+	
+
+	return campaignParts.has('en') ? 'english' : 'desktop';
+}
